41316cffd106d357bdb6e9cf5e889674d596941b
leonardhinganescu <hinganesculeonard@gmail.com>
2020-08-23 14:25:52 +0300

    Add files via upload

--- stuff/script.js
+++ stuff/script.js
@@ -1,26 +1,19 @@
+// Selectors
+const copyright = document.querySelector(".copyright");
+const expBtn 	= document.querySelector(".expandable");
+
+// Event Listeners
+document.addEventListener('DOMContentLoaded', showDate);
+expBtn.addEventListener('click', expandableSmth);
+
+
+// Functions
 $(function() {
-
-	var coll = document.getElementsByClassName("collapsible");
-	var i;
-
-	for (i = 0; i < coll.length; i++) {
-		coll[i].addEventListener("click", function() {
-			var content = this.nextElementSibling;
-			if (content.style.display === "block") {
-				content.style.display = "none";
-			} else {
-				content.style.display = "block";
-			}
-		});
-	}
-
-
-
 	var selectedClass = "";
 
 	$(".actiune").click(function(){ 
 
-		selectedClass = $(this).attr("data-rel"); 
+		selectedClass = $(this).attr("value"); 
 
 		var butoane_filtru = document.getElementsByClassName("actiune");
 
@@ -45,3 +38,23 @@ $(function() {
 	});
 
 });
+
+function expandableSmth(event) {
+	let content = this.nextElementSibling;
+
+	if (content.style.display === "block") {
+		content.style.display = "none";
+	} 
+	else {
+		content.style.display = "block";
+	}
+}
+
+function showDate(event) {
+	const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+	let footerText = "&copy " + monthNames[new Date().getMonth()] + " " + new Date().getFullYear();
+
+	footerText += ", cine e Leo?";
+
+	copyright.innerHTML = footerText; 
+}
