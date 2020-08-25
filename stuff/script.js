39d4c363b0154cabe77686866b34c3922391bca6
leonardhinganescu <hinganesculeonard@gmail.com>
2020-08-25 11:03:59 +0300

    Add files via upload

--- stuff/script.js
+++ stuff/script.js
@@ -1,60 +1,80 @@
 // Selectors
 const copyright = document.querySelector(".copyright");
-const expBtn 	= document.querySelector(".expandable");
+const expBtn = document.querySelector(".expandable");
 
 // Event Listeners
-document.addEventListener('DOMContentLoaded', showDate);
-expBtn.addEventListener('click', expandableSmth);
-
+document.addEventListener("DOMContentLoaded", showDate);
+if (expBtn) {
+  expBtn.addEventListener("click", expandableSmth);
+}
 
 // Functions
-$(function() {
-	var selectedClass = "";
+$(function () {
+  var selectedClass = "";
 
-	$(".actiune").click(function(){ 
+  $(".actiune").click(function () {
+    selectedClass = $(this).attr("value");
 
-		selectedClass = $(this).attr("value"); 
+    var butoane_filtru = document.getElementsByClassName("actiune");
 
-		var butoane_filtru = document.getElementsByClassName("actiune");
+    var i;
+    for (i = 0; i < butoane_filtru.length; i++) {
+      butoane_filtru[i].classList.remove("butonactiv");
+    }
+    this.classList.toggle("butonactiv");
 
-		var i;
-		for (i = 0; i < butoane_filtru.length; i++) {
-			butoane_filtru[i].classList.remove("butonactiv");
-		}
-		this.classList.toggle("butonactiv");
+    $("#portfolio").fadeTo(100, 0);
 
-		$("#portfolio").fadeTo(100, 0);
+    $("#portfolio div")
+      .not("." + selectedClass)
+      .delay(200)
+      .fadeOut()
+      .removeClass("scale-anm");
 
-		$("#portfolio div").not("."+selectedClass).delay(200).fadeOut().removeClass('scale-anm');
+    setTimeout(function () {
+      $("." + selectedClass)
+        .fadeIn()
+        .addClass("scale-anm");
+    }, 100);
 
-		setTimeout(function() {
-			$("."+selectedClass).fadeIn().addClass('scale-anm');
-		}, 100); 
-
-		setTimeout(function() {
-			$("#portfolio").fadeTo(1000, 1);
-		}, 500);
-
-	});
-
+    setTimeout(function () {
+      $("#portfolio").fadeTo(1000, 1);
+    }, 500);
+  });
 });
 
 function expandableSmth(event) {
-	let content = this.nextElementSibling;
+  let content = this.nextElementSibling;
 
-	if (content.style.display === "block") {
-		content.style.display = "none";
-	} 
-	else {
-		content.style.display = "block";
-	}
+  if (content.style.display === "block") {
+    content.style.display = "none";
+  } else {
+    content.style.display = "block";
+  }
 }
 
 function showDate(event) {
-	const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-	let footerText = "&copy " + monthNames[new Date().getMonth()] + " " + new Date().getFullYear();
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let footerText =
+    "&copy " +
+    monthNames[new Date().getMonth()] +
+    " " +
+    new Date().getFullYear();
 
-	footerText += ", cine e Leo?";
+  footerText += ", cine e Leo?";
 
-	copyright.innerHTML = footerText; 
+  copyright.innerHTML = footerText;
 }
