183377d78513b825e6afff27a5bf8f7f593adf4d
leonardhinganescu <hinganesculeonard@gmail.com>
2020-09-13 23:42:09 +0300

    Add files via upload

--- stuff/js/login.js
+++ stuff/js/login.js
@@ -1,10 +1,11 @@
-const users = ["LiviaCarasel", "AnaRasanu", "EugenToma"];
-const passwords = ["8U93UsDu", "m6V29kIj", "tW6pCy6p"];
-const name = ["hi Livia C&#259;r&#259;&#351;el", "hi Ana R&#259;&#351;anu", "hi Eugen Toma"];
+const users = ["LiviaCarasel", "AnaRasanu", "EugenToma", "RuxandraFilimon"];
+const passwords = ["8U93UsDu", "m6V29kIj", "tW6pCy6p", "dmNM9L6l"];
+const name = ["hi Livia C&#259;r&#259;&#351;el", "hi Ana R&#259;&#351;anu", "hi Eugen Toma", "hi Ruxandra Filimon"];
 const links = [
   "https://matix.li/4a36297afa27",
   "https://matix.li/23511ddd71c1",
   "https://matix.li/dc0056408cad",
+  "https://matix.li/02b18fe08173"
 ];
 
 const loginBtn = document.querySelector("#loginbtn");
