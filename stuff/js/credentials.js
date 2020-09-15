ba4c77f887e7733b19f1a3cc7d1ce2c0d23c8554
leonardhinganescu <hinganesculeonard@gmail.com>
2020-09-15 20:58:47 +0300

    Add files via upload

--- stuff/js/credentials.js
+++ stuff/js/credentials.js
@@ -1,12 +1,20 @@
-const users = ["LiviaCarasel", "AnaRasanu", "EugenToma", "RuxandraFilimon", "CoraOprea"];
-const passwords = ["8U93UsDu", "m6V29kIj", "tW6pCy6p", "dmNM9L6l", "7x0GXlgK"];
-const name = ["hi Livia C&#259;r&#259;&#351;el", "hi Ana R&#259;&#351;anu", "hi Eugen Toma", "hi Ruxandra Filimon", "hi Cora Oprea"];
+const users = ["LiviaCarasel", "AnaRasanu", "EugenToma", "RuxandraFilimon", "CoraOprea", "Dora&Mara"];
+const passwords = ["8U93UsDu", "m6V29kIj", "tW6pCy6p", "dmNM9L6l", "7x0GXlgK", "49hiFgRR"];
+const name = ["hi Livia C&#259;r&#259;&#351;el", "hi Ana R&#259;&#351;anu", "hi Eugen Toma", "hi Ruxandra Filimon", "hi Cora Oprea", "hi Dora & Mara"];
 const links = [
   "https://matix.li/4a36297afa27",
   "https://matix.li/23511ddd71c1",
   "https://matix.li/dc0056408cad",
   "https://matix.li/02b18fe08173",
   "#",
+  "#",
 ];
 
-const moodlinks = ["#", "#", "#", "#", "https://app.milanote.com/1Ki99v1EgoXu0a?p=ZxdJuAt95sS"];
+const moodlinks = [
+  "#",
+  "#",
+  "#",
+  "#",
+  "https://app.milanote.com/1Ki99v1EgoXu0a?p=ZxdJuAt95sS",
+  "https://app.milanote.com/1KiaLE1EgoXu0m?p=tnkFg59NQOR",
+];
