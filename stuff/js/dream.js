212e2b234f346fb1a56c5e77cf7b6255555c1a54
leonardhinganescu <hinganesculeonard@gmail.com>
2020-09-18 02:32:29 +0300

    Add files via upload

---
+++ stuff/js/dream.js
@@ -0,0 +1,162 @@
+const dream = [
+    { text: "Dreams become reality when we put our minds to it.", author: "Queen Latifah" },
+    {
+      text:
+        "Every great dream begins with a dreamer. Always remember, you have within you the strength, the patience, and the passion to reach for the stars to change the world.",
+      author: "Harriet Tubman",
+    },
+    { text: "A positive attitude can really make dreams come true - it did for me.", author: "David Bailey" },
+    { text: "The future belongs to those who believe in the beauty of their dreams.", author: "Eleanor Roosevelt" },
+    {
+      text:
+        "Keep your dreams alive. Understand to achieve anything requires faith and belief in yourself, vision, hard work, determination, and dedication. Remember all things are possible for those who believe.",
+      author: "Gail Devers",
+    },
+    { text: "The biggest adventure you can take is to live the life of your dreams.", author: "Oprah Winfrey" },
+    {
+      text:
+        "Stay true to yourself, yet always be open to learn. Work hard, and never give up on your dreams, even when nobody else believes they can come true but you. These are not cliches but real tools you need no matter what you do in life to stay focused on your path.",
+      author: "Phillip Sweet",
+    },
+    { text: "All our dreams can come true, if we have the courage to pursue them.", author: "Walt Disney" },
+    { text: "Hold fast to dreams, for if dreams die, life is a broken-winged bird that cannot fly.", author: "Langston Hughes" },
+    {
+      text:
+        "I believe that imagination is stronger than knowledge. That myth is more potent than history. That dreams are more powerful than facts. That hope always triumphs over experience. That laughter is the only cure for grief. And I believe that love is stronger than death.",
+      author: "Robert Fulghum",
+    },
+    { text: "Stay focused, go after your dreams and keep moving toward your goals.", author: "LL Cool J" },
+    {
+      text:
+        "Life is full of beauty. Notice it. Notice the bumble bee, the small child, and the smiling faces. Smell the rain, and feel the wind. Live your life to the fullest potential, and fight for your dreams.",
+      author: "Ashley Smith",
+    },
+    { text: "Trust in dreams, for in them is hidden the gate to eternity.", author: "Khalil Gibran" },
+    {
+      text: "Follow your passion, be prepared to work hard and sacrifice, and, above all, don't let anyone limit your dreams.",
+      author: "Donovan Bailey",
+    },
+    { text: "You have to dream before your dreams can come true.", author: "A. P. J. Abdul Kalam" },
+    { text: "Friends... they cherish one another's hopes. They are kind to one another's dreams.", author: "Henry David Thoreau" },
+    { text: "Follow your dreams, believe in yourself and don't give up.", author: "Rachel Corrie" },
+    { text: "Let us make our future now, and let us make our dreams tomorrow's reality.", author: "Malala Yousafzai" },
+    { text: "I'm a dreamer. I have to dream and reach for the stars, and if I miss a star then I grab a handful of clouds.", author: "Mike Tyson" },
+    { text: "I like the dreams of the future better than the history of the past.", author: "Thomas Jefferson" },
+    {
+      text:
+        "Consult not your fears but your hopes and your dreams. Think not about your frustrations, but about your unfulfilled potential. Concern yourself not with what you tried and failed in, but with what it is still possible for you to do.",
+      author: "Pope John XXIII",
+    },
+    {
+      text:
+        "We all have dreams. But in order to make dreams come into reality, it takes an awful lot of determination, dedication, self-discipline, and effort.",
+      author: "Jesse Owens",
+    },
+    {
+      text: "Deep into that darkness peering, long I stood there, wondering, fearing, doubting, dreaming dreams no mortal ever dared to dream before.",
+      author: "Edgar Allan Poe",
+    },
+    { text: "I couldn't find the sports car of my dreams, so I built it myself.", author: "Ferdinand Porsche" },
+    {
+      text: "Never give up on what you really want to do. The person with big dreams is more powerful than the one with all the facts.",
+      author: "H. Jackson Brown, Jr.",
+    },
+    { text: "A dream you dream alone is only a dream. A dream you dream together is reality.", author: "Yoko Ono" },
+    { text: "Reality is wrong. Dreams are for real.", author: "Tupac Shakur" },
+    { text: "Help others achieve their dreams and you will achieve yours.", author: "Les Brown" },
+    { text: "Through hard work, perseverance and a faith in God, you can live your dreams.", author: "Ben Carson" },
+    {
+      text:
+        "Remember your dreams and fight for them. You must know what you want from life. There is just one thing that makes your dream become impossible: the fear of failure.",
+      author: "Paulo Coelho",
+    },
+    { text: "Job training empowers people to realize their dreams and improve their lives.", author: "Sylvia Mathews Burwell" },
+    { text: "Yesterday is but today's memory, and tomorrow is today's dream.", author: "Khalil Gibran" },
+    { text: "A city is not gauged by its length and width, but by the broadness of its vision and the height of its dreams.", author: "Herb Caen" },
+    { text: "I dream of painting and then I paint my dream.", author: "Vincent Van Gogh" },
+    { text: "Dream as if you'll live forever. Live as if you'll die today.", author: "James Dean" },
+    {
+      text:
+        "We have to stand up for what we believe in, even when we might not be popular for it. Honesty starts with being ourselves, authentic and true to who we are and what we believe in, and that may not always be popular, but it will always let you follow your dreams and your heart.",
+      author: "Tabatha Coffey",
+    },
+    { text: "Who looks outside, dreams; who looks inside, awakes.", author: "Carl Jung" },
+    { text: "A heart without dreams is like a bird without feathers.", author: "Suzy Kassem" },
+    {
+      text:
+        "In my dreams, I could be a Princess, and that's what I was. Like most little girls, I believed nothing less than a Prince could make my dreams come true.",
+      author: "Loretta Young",
+    },
+    { text: "Why does the eye see a thing more clearly in dreams than the imagination when awake?", author: "Leonardo da Vinci" },
+    {
+      text:
+        "Throw your dreams into space like a kite, and you do not know what it will bring back, a new life, a new friend, a new love, a new country.",
+      author: "Anais Nin",
+    },
+    {
+      text:
+        "Never underestimate the power of dreams and the influence of the human spirit. We are all the same in this notion: The potential for greatness lives within each of us.",
+      author: "Wilma Rudolph",
+    },
+    { text: "The only thing that comes to a sleeping man is dreams.", author: "Tupac Shakur" },
+    {
+      text:
+        "My imagination completely controls me, and forever feeds the fire that burns with dark red light in my heart by bringing me the best dreams. I've always had a wild imagination, a big heart and a tortured soul so I feel that dark fantasy, love and horror are in my blood.",
+      author: "Kim Elizabeth",
+    },
+    {
+      text: "A dreamer is one who can only find his way by moonlight, and his punishment is that he sees the dawn before the rest of the world.",
+      author: "Oscar Wilde",
+    },
+    {
+      text:
+        "Follow your dreams. If you have a goal, and you want to achieve it, then work hard and do everything you can to get there, and one day it will come true.",
+      author: "Lindsey Vonn",
+    },
+    { text: "Let your dreams outgrow the shoes of your expectations.", author: "Ryunosuke Satoro" },
+    {
+      text: "Your vision will become clear only when you can look into your own heart. Who looks outside, dreams; who looks inside, awakes.",
+      author: "Carl Jung",
+    },
+    {
+      text:
+        "The path from dreams to success does exist. May you have the vision to find it, the courage to get on to it, and the perseverance to follow it.",
+      author: "Kalpana Chawla",
+    },
+  ];
+
+const quoteIndex = Math.floor(Math.random() * dream.length);
+const theAuthor = "- " + dream[quoteIndex].author;
+let theText = dream[quoteIndex].text.split(" ");
+
+for (let i = 0; i < theText.length; i++) {
+  if (theText[i].indexOf("dream") !== -1) {
+    theText[i] = '<b class="format-dream">' + theText[i] + "</b>";
+  }
+  if (theText[i].indexOf("Dream") !== -1) {
+    theText[i] = '<b class="format-dream">' + theText[i] + "</b>";
+  }
+}
+
+theText = theText.join(" ");
+
+const quoteDiv = document.querySelector(".quote");
+
+const columnDiv = document.createElement("div");
+columnDiv.classList.add("column-xs-12");
+columnDiv.classList.add("order3");
+quoteDiv.appendChild(columnDiv);
+
+const containerDiv = document.createElement("div");
+containerDiv.classList.add("dream-container");
+columnDiv.appendChild(containerDiv);
+
+const textP = document.createElement("p");
+textP.classList.add("dream-text");
+textP.innerHTML = theText;
+containerDiv.appendChild(textP);
+
+const authorP = document.createElement("p");
+authorP.classList.add("dream-author");
+authorP.innerHTML = theAuthor;
+containerDiv.appendChild(authorP);
