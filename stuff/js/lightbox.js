51a79a280c9826a1584958ce264455805a813236
leonardhinganescu <hinganesculeonard@gmail.com>
2021-04-24 13:00:44 +0300

    Add files via upload

--- stuff/js/lightbox.js
+++ stuff/js/lightbox.js
@@ -1,4 +1,5 @@
 let tiles;
+
 if (!(detectMob() || mobileCheck())) {
   tiles = document.querySelectorAll(".tile");
 }
@@ -6,6 +7,10 @@ const lightbox = document.querySelector(".lightbox");
 const lightboxImg = document.querySelector("#lightboxIMG");
 const body = document.body;
 let index;
+
+//---
+let selectedClass = ".all";
+//---
 
 for (let i = 0; i < tiles.length; i++) {
   tiles[i].addEventListener("click", function () {
@@ -81,4 +86,83 @@ function mobileCheck() {
       check = true;
   })(navigator.userAgent || navigator.vendor || window.opera);
   return check;
+}
+
+
+//    SELECTORS
+const expBtn = document.querySelector(".expandable");
+
+//    EVENT LISTENERS
+if (expBtn) expBtn.addEventListener("click", expandableSmth);
+
+//    Filtre
+$(function () {
+
+  $(".actiune").click(function () {
+    selectedClass = $(this).attr("value");
+
+    var butoane_filtru = document.getElementsByClassName("actiune");
+
+    var i;
+    for (i = 0; i < butoane_filtru.length; i++) {
+      butoane_filtru[i].classList.remove("butonactiv");
+    }
+    this.classList.toggle("butonactiv");
+
+    $("#portfolio").fadeTo(100, 0);
+
+    $("#portfolio div")
+      .not("." + selectedClass)
+      .delay(200)
+      .fadeOut()
+      .removeClass("scale-anm");
+
+    setTimeout(function () {
+      $("." + selectedClass)
+        .fadeIn()
+        .addClass("scale-anm");
+    }, 100);
+
+    setTimeout(function () {
+      $("#portfolio").fadeTo(1000, 1);
+    }, 500);
+
+    setTimeout(function () {
+      if (!(detectMob() || mobileCheck())) {
+        tiles = document.querySelectorAll(".scale-anm");
+      }
+  
+      console.log(tiles);
+  
+      for (let i = 0; i < tiles.length; i++) {
+        tiles[i].addEventListener("click", function () {
+          index = i;
+          changeImage();
+          lightBOX();
+        });
+      }
+    }, 1000);
+
+  });
+});
+
+//    Expand
+function expandableSmth(event) {
+  let content = this.nextElementSibling;
+
+  if (content.style.display === "block") {
+    content.style.display = "none";
+  } else {
+    content.style.display = "block";
+  }
+}
+
+
+//    scroll up button
+const scrollUpBtn = document.querySelector(".scrollUpBtn");
+
+scrollUpBtn.addEventListener("click", scrollUp);
+
+function scrollUp(event) {
+  window.scrollTo(0, 0);
 }
